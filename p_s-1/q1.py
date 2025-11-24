f8c225be02275d52577c386d4f9992bee4c3bb34
Nikunjsolanki <nikunjsingh376@gmail.com>
2025-11-24 19:29:56 +0530

    new file add

---
+++ p_s-1/q1.py
@@ -0,0 +1,7 @@
+# print poem with input fom user!
+p = "Im turning the world of poetry over to Franny Choi" \
+"Im leaving the world of poetry to Franny Choi" \
+"Im giving the world of poetry to Franny Choi"\
+"I feel confident leaving the world of poetry to Franny Choi"\
+"Im not sorry to give..."
+print(p)
---
+++ p_s-1/q3.py
@@ -0,0 +1,6 @@
+import pyttsx3 as p3
+
+words = input("enter do you want to speech: ")
+p3.speak(words)
+
+# text to speech program
