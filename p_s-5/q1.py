8a4a3d79c35cbf91db44fdb57b8aaa6bcae5a2a7
Nikunjsolanki <nikunjsingh376@gmail.com>
2025-11-24 17:57:12 +0530

    q added

---
+++ p_s-5/q1.py
@@ -0,0 +1,10 @@
+# make a dictonary with key with their hindi transalted words!
+words = {
+    "help" : "madad",
+    "months" :"mahina",
+    "cat" : "billi",
+    "Dog" : "kutta"
+
+}
+word = input("enter word to get english meaning: ")
+print(words[word])
---
+++ p_s-5/q2.py
@@ -0,0 +1,11 @@
+# input 8 numbers from user and print unique numbers!
+numbers = set()
+for i in range(8):
+
+ number = int(input("enter numbers: "))
+
+ numbers.add(number)
+
+
+print(numbers)
+
